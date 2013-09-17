f1b044f19f1657173b5b0bbf50e616834262459d
njohnson41 <nick.johnson@gtri.gatech.edu>
2013-09-16 22:04:49 -0400

    scheduler accepting new matches from HTTP interface

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -9,7 +9,7 @@ import tornado.web
 
 ioloop = tornado.ioloop.IOLoop.instance()
 
-match_servers = []
+match_servers = {}
 
 class Scheduler(object):
 
@@ -30,7 +30,6 @@ class Scheduler(object):
 
     @classmethod
     def schedule(cls, match):
-        print 'scheduling ' + match.match_id
         if len(cls.worker_queue) > 0:
             worker = cls.worker_queue.pop(0)
             worker.assign_match(match)
@@ -46,7 +45,6 @@ class MatchWorker(object):
         Scheduler.add_worker(self)
 
     def assign_match(self, match):
-        print 'worker assigned match ' + match.match_id
         self.match = match
         self.busy = True
         self.match.start(self)
@@ -63,7 +61,12 @@ class MatchTask(object):
 
     index = {}
 
-    def __init__(self, match_id, game, players, config):
+    @classmethod
+    def generate_match_id(cls):
+        return base64.urlsafe_b64encode(os.urandom(12))
+
+    def __init__(self, game, players, config):
+        match_id = MatchTask.generate_match_id()
         MatchTask.index[match_id] = self
         self.match_id = match_id
         self.game = game
@@ -80,14 +83,13 @@ class MatchTask(object):
     def log(self, log):
         self.logs.append(log)
 
-    def run(self, match_id, game_key, player_keys):
+    def run(self, match_id, game_key, player_keys, config):
         self.match_id = match_id
         self.game_key = game_key
         self.player_keys = player_keys
         self.state = 'running'
 
     def finish(self, results):
-        print '%s finished with %s' % (self.match_id, json.dumps(results))
         self.results = results
         self.state = 'finished'
         self.worker.free()
@@ -165,26 +167,27 @@ class MatchServer(object):
             for i in range(message[2]['workers']):
                 self.workers.append(MatchWorker(self))
             self.ready = True
-        elif message[0] == 'END':
-            match_id = message[2]
-            results = message[3]
-            self.matches[match_id].finish(results)
         elif message[0] == 'FIN':
             self.on_close()
-        else:
-            match_id = message[2]
-            if match_id in self.matches:
-                self.matches[match_id].log(message)
+        elif message[2] in MatchTask.index:
+            match = MatchTask.index[message[2]]
+            if message[0] == 'NEW':
+                match.run(*message[2:])
+            elif message[0] == 'END':
+                match.finish(*message[3:])
+            else:
+                match.log(message)
 
     def on_close(self):
         for worker in self.workers:
             worker.busy = False
         for match in self.matches.values():
             match.redo()
-        self.active_matches = {}
+        self.matches = {}
 
     def remove_match(self, match_id):
-        del self.matches[match_id]
+        if match_id in self.matches:
+            del self.matches[match_id]
 
     def schedule_match(self, match, worker):
         self.send({'$schedule':{
@@ -195,15 +198,18 @@ class MatchServer(object):
             }})
         self.matches[match.match_id] = match
 
+games = {}
+agents = {}
+
 #
 # Tornado HTTP server
 #
 
-class MatchServerHandler(tornado.websocket.WebSocketHandler):
+class MatchServerBackendHandler(tornado.websocket.WebSocketHandler):
 
     def open(self):
         self.match_server = MatchServer(self)
-        match_servers.append(self.match_server)
+        match_servers[self.request.remote_ip] = self.match_server
 
     def on_message(self, message):
         message = json.loads(message)
@@ -217,7 +223,23 @@ class MatchServerIndexHandler(tornado.web.RequestHandler):
 
     def get(self):
         self.set_header('content-type', 'application/json')
-        self.write(json.dumps({ server.hostname: [worker.busy for worker in server.workers] for server in match_servers}))
+        self.write(json.dumps(list(match_servers.keys())))
+
+class MatchServerHandler(tornado.web.RequestHandler):
+    
+    def get(self, hostname):
+        try:
+            server = match_servers[hostname]
+        except KeyError:
+            raise tornado.web.HTTPError(404)
+
+        self.set_header('content-type', 'application/json')
+        self.write(json.dumps({
+            'workers' : [
+                worker.match.match_id if worker.match else None
+                for worker in server.workers
+            ],
+        }))
 
 class MatchIndexHandler(tornado.web.RequestHandler):
     
@@ -225,66 +247,72 @@ class MatchIndexHandler(tornado.web.RequestHandler):
         self.set_header('content-type', 'application/json')
         self.write(json.dumps(list(MatchTask.index.keys())))
 
-class MatchRootHandler(tornado.web.RequestHandler):
+    def post(self):
+        try:
+            match_config = json.loads(self.request.body)
+            if len(match_config.keys()) != 3:
+                raise ValueError
+            game = match_config['game']
+            players = match_config['players']
+            config = match_config['config']
+        except ValueError, KeyError:
+            raise tornado.web.HTTPError(400)
+
+        match = MatchTask(game, players, config)
+        Scheduler.schedule(match)
+
+        self.set_header('content-type', 'application/json')
+        self.write(json.dumps(match.match_id))
+
+class MatchHandler(tornado.web.RequestHandler):
     
     def get(self, match_id):
         try:
-            match = matches[match_id]
+            match = MatchTask.index[match_id]
         except KeyError:
-            raise tornado.web.HTTPError(400)
-        self.write('')
+            raise tornado.web.HTTPError(404)
+
+        self.set_header('content-type', 'application/json')
+        self.write(json.dumps(match.document))
+
+    def delete(self, match_id):
+        try:
+            match = MatchTask.index[match_id]
+        except KeyError:
+            raise tornado.web.HTTPError(404)
+        del MatchTask.index[match_id]
 
 class MatchLogHandler(tornado.web.RequestHandler):
     
     def get(self, match_id):
         try:
-            match = matches[match_id]
+            match = MatchTask.index[match_id]
         except KeyError:
             raise tornado.web.HTTPError(400)
-        self.write('')
-
-class MatchLogListener(tornado.websocket.WebSocketHandler):
-    
-    def open(self):
-        pass
-
-    def on_message(self, message):
-        pass
-
-    def on_close(self):
-        pass
-
-class ScheduleQueueHandler(tornado.web.RequestHandler):
-    
-    def get(self):
-        self.set_header('content-type', 'application/json')
-        self.write('[]')
-
-    def post(self):
-        print self.request.body
-        self.write('')
+        self.write(json.dumps(sorted(match.logs, lambda l: l[1])))
 
 routes = [
-    (r'/match_server', MatchServerHandler),
 
-    (r'/schedule_queue', ScheduleQueueHandler),
+    # match server backend websocket endpoint
+    (r'/match_server', MatchServerBackendHandler),
 
+    # match server health/statistics
     (r'/match_servers/', MatchServerIndexHandler),
+    (r'/match_servers/([-_a-zA-Z0-9.]+)', MatchServerHandler),
 
+    # game and agent collections
+    (r'/games/', GameIndexHandler),
+    (r'/games/([-_a-zA-Z0-9.]+)', GameHandler),
+    (r'/agents/([-_a-zA-Z0-9.]+)/', AgentIndexHandler),
+    (r'/agents/([-_a-zA-Z0-9.]+)/([a-f0-9]+)', AgentHandler),
+
+    # match tracking
     (r'/matches/', MatchIndexHandler),
-    (r'/matches/([-_a-zA-Z0-9]+)', MatchRootHandler),
+    (r'/matches/([-_a-zA-Z0-9]+)', MatchHandler),
     (r'/matches/([-_a-zA-Z0-9]+)/logs', MatchLogHandler),
-    (r'/matches/([-_a-zA-Z0-9]+)/logs', MatchLogListener),
 ]
 
-def schedule_test():
-    if Scheduler.task_queue == []:
-        Scheduler.schedule(MatchTask(base64.urlsafe_b64encode(os.urandom(6)), 'rock_paper_scissors', ['a'] * 2, {}))
-
 if __name__ == '__main__':
-
-    tornado.ioloop.PeriodicCallback(schedule_test, 100).start()
-
     app = tornado.web.Application(routes)
     app.listen(4200)
     ioloop.start()
